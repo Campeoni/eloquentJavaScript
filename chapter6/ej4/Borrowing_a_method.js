e981749f5b091cdeafa4645c54820408016897df
Campeoni <nicolas.campeoni@gmail.com>
2022-02-09 12:12:05 -0300

    20220209

---
+++ chapter6/ej4/Borrowing_a_method.js
@@ -0,0 +1,6 @@
+let map = {one: true, two: true, hasOwnProperty: true};
+
+console.log(Object.prototype.hasOwnProperty.call(map,"try"));
+// → true
+
+
---
+++ chapter7/ej1/A_vector_type.js
@@ -0,0 +1,32 @@
+// Your code here.
+class Vec {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vec) {
+    vec.x = this.x + vec.x;
+    vec.y = this.y + vec.y;
+
+    return vec;    
+  } 
+
+  minus(vec) {
+    vec.x = this.x - vec.x;
+    vec.y = this.y - vec.y;
+
+    return vec;        
+  }
+
+  get length() {
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+} 
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// → 5
